90917446653197634708ca7d737963d126cab257
EvArk <arkalis.e@gmail.com>
2016-07-04 04:09:04 +0300

    Blink example is back

--- examples/arduino_blink/arduino_blink.ino
+++ examples/arduino_blink/arduino_blink.ino
@@ -1,15 +1,11 @@
-uint32_t    n=0;
-
-void setup()
-{
-    pinMode(PC13, OUTPUT);
-    Serial1.begin(115200);
-    Serial1.println(F("-PROGRAM START-"));
+void setup() {
+  // initialize digital pin 13 as an output.
+  pinMode(PC13, OUTPUT);
 }
 
-void loop()
-{
-    Serial1.println(n++);
-    togglePin(PC13);
-    delay(100);            
+void loop() {
+  digitalWrite(PC13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                // wait for a second
+  digitalWrite(PC13, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);             
 }
