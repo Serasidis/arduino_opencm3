269fc0b3cb33b763a1c507f2532a057ad20dce1b
EvArk <arkalis.e@gmail.com>
2016-07-25 01:32:59 +0300

    SPI fixed path

---
+++ stm32/libraries/cm3_examples/examples/serial_ex2/serial_ex2.ino
@@ -0,0 +1,24 @@
+uint32_t    n=0;
+
+void setup()
+{
+
+    pinMode(LED1, OUTPUT);
+    Serial.begin(115200);
+    Serial.println(F("-PROGRAM START-"));
+    Serial.print(" SYSTEM CLOCK:");
+    Serial.println( sysclk() );
+    Serial.print(" APB1 CLOCK:");
+    Serial.println( pb1clk() );
+    Serial.print(" APB2 CLOCK:");
+    Serial.println( pb2clk() );
+}
+
+void loop()
+{
+    Serial.print(n++);
+    Serial.print(" RSV:");
+    Serial.println( Serial.available());
+    togglePin(LED1);
+    delay(100);
+}
